dfcd738582f164be838d0428d12ade34f958ec9f
HanningtoneM <hanningtonepompyy@gmail.com>
2023-08-08 17:02:13 +0300

    Add boxshadow function

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ const blurRadius = document.querySelector(".blur-radius")
 const blurSpread = document.querySelector(".blur-spread")
 const color = document.querySelector(".color")
 const cssValue = document.querySelector(".css-value")
+
+function updateBoxShadow(){
+    const v = `${x.value}px ${y.value}px ${blurRadius.value}px ${blurSpread.value}px ${color.value}`
+}
