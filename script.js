3ac15fcb53e74cfd1046f46fb8e6a8cf68a51d72
HanningtoneM <hanningtonepompyy@gmail.com>
2023-08-09 15:22:10 +0300

    format js code

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const box = document.querySelector(".box")
-const x = document.querySelector(".offset-x")
-const y = document.querySelector(".offset-y")
-const blurRadius = document.querySelector(".blur-radius")
-const blurSpread = document.querySelector(".blur-spread")
-const color = document.querySelector(".color")
-const cssValue = document.querySelector(".css-value")
+const box = document.querySelector(".box");
+const x = document.querySelector(".offset-x");
+const y = document.querySelector(".offset-y");
+const blurRadius = document.querySelector(".blur-radius");
+const blurSpread = document.querySelector(".blur-spread");
+const color = document.querySelector(".color");
+const cssValue = document.querySelector(".css-value");
 
-function updateBoxShadow(){
-    const v = `${x.value}px ${y.value}px ${blurRadius.value}px ${blurSpread.value}px ${color.value}`
+function updateBoxShadow() {
+	const v = `${x.value}px ${y.value}px ${blurRadius.value}px ${blurSpread.value}px ${color.value}`;
 
-    cssValue.value = v;
-    box.style.boxShadow = v;
+	cssValue.value = v;
+	box.style.boxShadow = v;
 }
 
-[x, y, blurRadius, blurSpread, color].forEach(element =>{
-    element.oninput = updateBoxShadow;
-})
- updateBoxShadow()
+[x, y, blurRadius, blurSpread, color].forEach((element) => {
+	element.oninput = updateBoxShadow;
+});
+updateBoxShadow();
